c38c0f9a913309ededacd1ca3e787e98c8243656
Nikolay Karataev <nikolay@karataev.net>
2016-11-29 16:59:58 -0500

    added libriaries

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+var path = require('path');
+var CLIENT_PATH = '/client_src';
+app.use ('/', express.static(path.join(__dirname, CLIENT_PATH)));
+var server = app.listen(3000, function (){
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('listening %s:%s',host,port);
+})
